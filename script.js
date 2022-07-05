4328a1094725409a1dca4dd0e414785a5035573c
Igor <GavrilinIV.mail@yandex.ru>
2022-07-06 02:07:48 +0300

    first commit, lesson 19

---
+++ script.js
@@ -0,0 +1,27 @@
+'strict mode';
+
+const numberOfFilms = +prompt ('Сколько фильмов Вы уже посмотрели?', '');
+
+const personalMovieDb = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+// for (let i = 1; i < 3; i++){
+// }
+
+
+    const answer1 = prompt("Один из псоледних просмотренных фильмов", ''),
+          answer2 = prompt("На сколько оцените его?", ),
+          answer3 = prompt("Один из псоледних просмотренных фильмов", ''),
+          answer4 = prompt("На сколько оцените его?", );
+
+    personalMovieDb.movies[answer1] = answer2;
+    personalMovieDb.movies[answer3] = answer4;
+
+
+
+console.log(personalMovieDb);
