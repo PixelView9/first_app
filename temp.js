bc433348b03db751b30949255bee4a48552365cb
Igor <GavrilinIV.mail@yandex.ru>
2022-07-23 01:02:47 +0300

    first task done

--- temp.js
+++ temp.js
@@ -1,4 +1,17 @@
 'strict mode';
 
-let str = '';
-console.log( 'Österreich'.localeCompare('Zealand') );
+function calculate(length){
+
+    // console.log(typeof(length));
+    // console.log(typeof(length) !== 'number');
+    // console.log(parseInt(length) != length);
+
+    if(typeof(length) !== 'number' || parseInt(length) != length || length < 0) {
+        return 'При вычислении произошла ошибка';
+    } else {
+        return (`Объем куба: ${length * length * length}, площадь всей поверхности: ${length * length * 6}`);
+    }
+    
+}
+
+console.log(calculate(-15));
