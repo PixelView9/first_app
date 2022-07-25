661d5cf154b4bbc23cf04350f95bd6b6401461fe
Igor <GavrilinIV.mail@yandex.ru>
2022-07-26 01:54:29 +0300

    dfsdf

--- homeWork.js
+++ homeWork.js
@@ -3,19 +3,10 @@ function findMaxNumber(a, b, c, d){
 
     if(typeof(a) !== 'number' || typeof(b) !== 'number' || typeof(c) !== 'number' || typeof(d) !== 'number'){
         return 0;
+    } else {
+        return Math.max(a, b, c, d);
     }
 
-    let maxNumber = 0;
-    let arr = [a, b, c, d];
-
-    arr.forEach(element => {
-        if(element > maxNumber){
-            maxNumber = element;
-        }
-        
-    });
-
-    return maxNumber;
 }
 
-console.log(findMaxNumber(3, -15551, 501));
+console.log(findMaxNumber(3, -15551, 501, 15));
