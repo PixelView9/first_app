36e0f8f0c846ad2603bdd5268b7ece6b471e11c8
Igor <GavrilinIV.mail@yandex.ru>
2022-07-27 00:38:00 +0300

    fdsfs

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ function rememberMyFilms(){
 
     for (let i = 0; i < 2; i++) {
 
-        const a = prompt('Один из поcледних просмотренных фильмов', ''),
+        const a = prompt('Один из поcледних просмотренных фильмов', '').trim(),
             b = prompt('На сколько оцените его?', '');
     
         if (a != null && b != null && a != '' && b != '' && a.length < 50 && b.length < 50) {
