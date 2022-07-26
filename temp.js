63f00d23ae1c47556767aeac62c18add653dd8c5
Igor <GavrilinIV.mail@yandex.ru>
2022-07-27 01:56:27 +0300

    fdsfs

--- temp.js
+++ temp.js
@@ -1,4 +1,12 @@
 'strict mode';
 
-console.log('fdfsf'.toUpperCase());
-console.log('TTTRRER'.toLowerCase());
+function extractCurrencyValue(str){
+    let ds = str.slice(1);
+    console.log(ds);
+    console.log(typeof(ds));
+    console.log(Number(ds));
+    console.log(typeof(Number(ds)));
+    return Number(ds);
+}
+
+console.log(extractCurrencyValue(`$120`));
