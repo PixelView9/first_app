d89f0f4a1d7488cb16ff1866f7b8c6b53a5e4911
Igor <GavrilinIV.mail@yandex.ru>
2022-07-30 02:03:13 +0300

    dfsf

--- temp.js
+++ temp.js
@@ -1,14 +1,27 @@
 'strict mode';
 
-function extractCurrencyValue(str){
-    let ds = str.slice(1);
-    console.log(ds);
-    console.log(typeof(ds));
-    console.log(Number(ds));
-    console.log(typeof(Number(ds)));
-    return Number(ds);
+const options = {
+    name: `test`,
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    }
+
+};
+// delete options.name;
+
+console.log(options);
+
+for (let key in options) {
+    if (typeof(options[key]) === 'object'){
+        for (let i in key){
+        console.log(`Свойство ${i}: ${options[key][i]}`);
+        }
+    }
+
+    console.log(`Свойство ${key}: ${options[key]}`);
 }
 
-console.log(extractCurrencyValue(`$120`));
 
-console.log();
